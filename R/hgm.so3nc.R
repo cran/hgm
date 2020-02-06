aa803809216662520728d0d8e925c5a89d9c1643
Nobuki Takayama <takayama@math.kobe-u.ac.jp>
2020-02-06 11:10:06 +0000

    version 1.18

--- R/hgm.so3nc.R
+++ R/hgm.so3nc.R
@@ -1,10 +1,12 @@
-# $OpenXM: OpenXM/src/R/r-packages/hgm/R/hgm.so3nc.R,v 1.5 2014/03/22 00:15:40 takayama Exp $
+# $OpenXM: OpenXM/src/R/r-packages/hgm/R/hgm.so3nc.R,v 1.6 2019/11/16 11:03:43 takayama Exp $
 "hgm.tk.so3nc" <-
-function(a=1,b=2,c=3,t0=0.0,q=1,deg=0) { 
+function(a=1,b=2,c=3,t0=0.0,q=1,deg=0,log=0) { 
 #  library.dynam("mypkg",package="mypkg",lib.loc="./");
   if (!is.loaded("hgm")) library.dynam("hgm",package="hgm",lib.loc=NULL);
+  ans <-
   .C("so3_main",as.double(a),as.double(b),as.double(c),
      as.double(t0),
-     as.integer(q),as.integer(deg),
-     retv=as.double(0),PACKAGE="hgm")$retv
+     as.integer(q),as.integer(deg),as.integer(log),
+     retv=double(4),PACKAGE="hgm")$retv;
+  return(ans); 
 }
